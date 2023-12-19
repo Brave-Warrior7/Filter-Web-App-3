2fb325182150bc8e6337b9aa2d790c65c452e65f
Brave-Warrior7 <134350321+Brave-Warrior7@users.noreply.github.com>
2023-12-18 19:57:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+nose_x=0;
+nose_y=0;
+
+function preload(){
+mustache=loadImage('https://i.postimg.cc/tgGmnCnH/mustache-removebg-preview.png');
+beard=loadImage('https://i.postimg.cc/T3bC8qTB/images-removebg-preview.png');
+}
+
+
+function setup(){
+    canvas=createCanvas(400,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+ image(video, 0, 0, 400, 300);
+ image(mustache, nose_x+20, nose_y-20, 70, 70); 
+ image(beard, nose_x+10, nose_y, 90, 90); 
+
+}
+
+function take_snapshot(){
+    save('myFilter.png');
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+ if(results.length > 0){
+     console.log(results);
+     nose_x=results[0].pose.nose.x;
+     nose_y=results[0].pose.nose.y;
+     console.log("nose-x is", nose_x);
+     console.log("nose-y is", nose_y);
+ }
+
+}
